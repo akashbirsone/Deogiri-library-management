cfd7eadc714206a49dceb6d1214f9f7e33c95c2c
Akash Birsone <akashbirsone80@gmail.com>
2025-10-03 05:44:34 +0000

    change the logo name and including the new name Deogiri Library Manageme

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -2,12 +2,12 @@ import { Library } from "lucide-react";
 
 export function Logo() {
   return (
-    <div className="flex items-center gap-2" aria-label="Deogiri e-Granthalaya Home">
+    <div className="flex items-center gap-2" aria-label="Deogiri Library Management Home">
       <div className="bg-primary text-primary-foreground p-2 rounded-lg">
         <Library className="h-5 w-5" />
       </div>
       <span className="font-headline text-lg font-semibold min-w-max">
-        Deogiri e-Granthalaya
+        Deogiri Library Management
       </span>
     </div>
   );
