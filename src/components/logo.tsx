e7a705d09471e79c4e196081e9ec99c63d6d1d14
Akash Birsone <akashbirsone80@gmail.com>
2025-10-03 11:24:34 +0000

    this text size apply for small device  15px (_for element <Logo>_)

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -6,7 +6,7 @@ export function Logo() {
       <div className="bg-primary text-primary-foreground p-2 rounded-lg">
         <Library className="h-5 w-5" />
       </div>
-      <span className="font-headline text-[30px] font-semibold min-w-max transition-opacity duration-200 group-data-[collapsible=icon]:opacity-0 group-data-[collapsible=icon]:w-0">
+      <span className="font-headline text-[20px] md:text-[30px] font-semibold min-w-max transition-opacity duration-200 group-data-[collapsible=icon]:opacity-0 group-data-[collapsible=icon]:w-0">
         Deogiri Library Management
       </span>
     </div>
