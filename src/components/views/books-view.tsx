0c4c09b82f8415065d8f178feac3261d7baa380f
Akash Birsone <akashbirsone80@gmail.com>
2025-11-30 09:31:29 +0000

    Try fixing this error: `Runtime Error: Error: CardDescription is not def

--- src/components/views/books-view.tsx
+++ src/components/views/books-view.tsx
@@ -6,6 +6,7 @@ import Image from "next/image";
 import {
   Card,
   CardContent,
+  CardDescription,
   CardFooter,
   CardHeader,
   CardTitle,
@@ -543,5 +544,3 @@ const BookForm = ({
     </DialogContent>
   );
 };
-
-    
